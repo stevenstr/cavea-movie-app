0ba274ed33f048377bfd3621433283bde07a552e
Stevenstr <stephenzhavrid@gmail.com>
2025-01-23 18:10:41 +0300

    fixes..

---
+++ go.mod
@@ -0,0 +1,3 @@
+module movie.com
+
+go 1.23.3
--- metadata/internal/repository/memory/memory.go
+++ metadata/internal/repository/memory/memory.go
@@ -1,18 +1,31 @@
 package memory
 
 import (
-	"mymovie_applctn/metadata/pkg/model"
+	"context"
 	"sync"
+
+	"movie.com/metadata/internal/repository"
+	"movie.com/metadata/pkg/model"
 )
 
 type Repository struct {
 	sync.RWMutex
-	data map[string]*model.MetaData
+	data map[string]*model.Metadata
 }
 
 func New() *Repository {
-	return &Repository{data: map[string]*model.MetaData{}}
+	return &Repository{data: map[string]*model.Metadata{}}
 }
 
-func Get() {}
+func (r *Repository) Get(_ context.Context, id string) (*model.Metadata, error) {
+	r.RLock()
+	defer r.RUnlock()
+
+	m, ok := r.data[id]
+	if !ok {
+		return nil, repository.ErrNotFound
+	}
+	return m, nil
+
+}
 func Put() {}
