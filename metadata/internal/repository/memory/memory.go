de0a91e3faa3978a09803109c6d47be496ae2dfb
Stevenstr <stephenzhavrid@gmail.com>
2025-01-23 18:16:19 +0300

    metadata repository with implementation of new get and put methods.

--- metadata/internal/repository/memory/memory.go
+++ metadata/internal/repository/memory/memory.go
@@ -8,15 +8,18 @@ import (
 	"movie.com/metadata/pkg/model"
 )
 
+// Repository defines in memory reprisentation set of metadata.
 type Repository struct {
 	sync.RWMutex
 	data map[string]*model.Metadata
 }
 
+// New create new repository in memory.
 func New() *Repository {
 	return &Repository{data: map[string]*model.Metadata{}}
 }
 
+// Get retrive the metadata from repo by id.
 func (r *Repository) Get(_ context.Context, id string) (*model.Metadata, error) {
 	r.RLock()
 	defer r.RUnlock()
@@ -28,4 +31,13 @@ func (r *Repository) Get(_ context.Context, id string) (*model.Metadata, error) 
 	return m, nil
 
 }
-func Put() {}
+
+// Put add the new metadata by proven id.
+func (r *Repository) Put(_ context.Context, id string, metadata *model.Metadata) error {
+	r.Lock()
+	defer r.Unlock()
+
+	r.data[id] = metadata
+
+	return nil
+}
