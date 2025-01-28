d5c9a91c1030f9ae470feda80a92ddca2cd91426
Stevenstr <stephenzhavrid@gmail.com>
2025-01-28 17:38:10 +0300

    part of handlerating function

--- rating/internal/handler/http/http.go
+++ rating/internal/handler/http/http.go
@@ -4,16 +4,37 @@ import (
 	"net/http"
 
 	"movie.com/rating/internal/controller/rating"
+	"movie.com/rating/pkg/model"
 )
 
+// Handler defines rating service controller.
 type Handler struct {
 	ctrl *rating.Controller
 }
 
+// New creates a new rating service http handler.
 func New(ctrl *rating.Controller) *Handler {
 	return &Handler{ctrl: ctrl}
 }
 
-func HandleGetPutRating(w http.ResponseWriter, req *http.Request) {
+// HandleRating handles Get and Put methods by /rating uri requests.
+func HandleRating(w http.ResponseWriter, req *http.Request) {
+	recordID := model.RecordID(req.FormValue("id"))
+	if recordID == "" {
+		w.WriteHeader(http.StatusBadRequest) // 400
+		return
+	}
 
+	recordType := model.RecordType(req.FormValue("type"))
+	if recordType == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	switch req.Method {
+	case http.MethodGet:
+	case http.MethodPut:
+	default:
+		w.WriteHeader(http.StatusBadRequest) // 400
+	}
 }
