01cfeb7e6b76205dd4d46abe2345f89ada7786c8
Stevenstr <stephenzhavrid@gmail.com>
2025-01-28 18:07:55 +0300

    executor of reting service was implemented

--- rating/cmd/main.go
+++ rating/cmd/main.go
@@ -1,6 +1,22 @@
 package main
 
-func main() {
-	// repo := memory.New()
+import (
+	"log"
+	"net/http"
 
+	"movie.com/rating/internal/controller/rating"
+	httpHandler "movie.com/rating/internal/handler/http"
+	"movie.com/rating/internal/repository/memory"
+)
+
+func main() {
+	log.Println("Staring the movie rating service")
+	repo := memory.New()
+	ctrl := rating.New(repo)
+	h := httpHandler.New(ctrl)
+
+	http.Handle("/rating", http.HandlerFunc(h.HandleRating))
+	if err := http.ListenAndServe(":8082", nil); err != nil {
+		panic(err)
+	}
 }
