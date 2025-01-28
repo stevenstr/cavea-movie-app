26a121f97440ea4df9afdb4bcbbaa2053f80ec17
Stevenstr <stephenzhavrid@gmail.com>
2025-01-28 17:53:08 +0300

    handlerating method implementation

--- rating/internal/handler/http/http.go
+++ rating/internal/handler/http/http.go
@@ -1,7 +1,11 @@
 package http
 
 import (
+	"encoding/json"
+	"errors"
+	"log"
 	"net/http"
+	"strconv"
 
 	"movie.com/rating/internal/controller/rating"
 	"movie.com/rating/pkg/model"
@@ -18,7 +22,7 @@ func New(ctrl *rating.Controller) *Handler {
 }
 
 // HandleRating handles Get and Put methods by /rating uri requests.
-func HandleRating(w http.ResponseWriter, req *http.Request) {
+func (h *Handler) HandleRating(w http.ResponseWriter, req *http.Request) {
 	recordID := model.RecordID(req.FormValue("id"))
 	if recordID == "" {
 		w.WriteHeader(http.StatusBadRequest) // 400
@@ -33,7 +37,30 @@ func HandleRating(w http.ResponseWriter, req *http.Request) {
 
 	switch req.Method {
 	case http.MethodGet:
+		v, err := h.ctrl.GetAggregatedRating(req.Context(), recordID, recordType)
+		if err != nil && errors.Is(err, rating.ErrNotFound) {
+			w.WriteHeader(http.StatusNotFound) // 404
+			return
+		}
+
+		if err := json.NewEncoder(w).Encode(v); err != nil {
+			log.Printf("Response encode error: %v\n", err)
+		}
+
 	case http.MethodPut:
+		userID := model.UserID(req.FormValue("userId"))
+		v, err := strconv.ParseFloat(req.FormValue("value"), 64)
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if err := h.ctrl.PutRating(req.Context(), recordID, recordType, &model.Rating{UserID: userID, Value: model.RatingValue(v)}); err != nil {
+			log.Printf("Repository pu error: %v\n", err)
+			w.WriteHeader(http.StatusInternalServerError) // 50
+		}
+
 	default:
 		w.WriteHeader(http.StatusBadRequest) // 400
 	}
