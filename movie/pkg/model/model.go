948bfdfce589c60db1a2782513e8f054a1ddbae6
Stevenstr <stephenzhavrid@gmail.com>
2025-01-29 14:56:42 +0300

    controller was  implemented

--- movie/pkg/model/model.go
+++ movie/pkg/model/model.go
@@ -5,6 +5,6 @@ import "movie.com/metadata/pkg/model"
 // MovieDetails include movie metadata
 // and its aggregated reting.
 type MovieDetails struct {
-	Rating   float64        `json:"rating,omitempty"`
+	Rating   *float64       `json:"rating,omitempty"`
 	Metadata model.Metadata `json:"metadata"`
 }
