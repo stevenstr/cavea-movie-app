25355a0d5a72a57457884a6a8e1350089c75655b
Stevenstr <stephenzhavrid@gmail.com>
2025-01-29 22:19:20 +0300

    lolz

--- movie/internal/gateway/error.go
+++
@@ -1,6 +0,0 @@
-package gateway
-
-import "errors"
-
-// ErrNotFound will return if data not found.
-var ErrNotFound = errors.New("not found")
--- rating/internal/repository/error.go
+++
@@ -1,7 +0,0 @@
-package repository
-
-import "errors"
-
-// ErrNotFound is returned when a requested record
-// was not found.
-var ErrNotFound = errors.New("not found")
