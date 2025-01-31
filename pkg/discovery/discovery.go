639951b2c92e8806959212e6a7cd84e848405bce
Stevenstr <stephenzhavrid@gmail.com>
2025-01-31 18:16:00 +0300

    Create Registry for services and service instance ID generator

--- pkg/discovery/discovery.go
+++ pkg/discovery/discovery.go
@@ -1,5 +1,23 @@
 package discovery
 
-import "errors"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 var ErrNotFound = errors.New("service address not found")
+
+// Registry represents service registry.
+type Registry interface {
+	Register(ctx context.Context, instanceID, serviceName, hostPort string) error
+	Deregister(ctx context.Context, instanceID, seriviceName string) error
+	ServiceAddress(ctx context.Context, instanceID string) ([]string, error)
+	ReportHealthyState(instanceID, seriviceName string) error
+}
+
+func GenerateInstanceID(serviceName string) string {
+	return fmt.Sprintf("%s-%d", serviceName, rand.New(rand.NewSource(time.Now().UnixNano())).Int())
+}
